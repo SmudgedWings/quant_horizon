5ba1141d9525553f9658274e0d6b96305bc31781
helloyongyang <yongyang1030@163.com>
2024-12-06 15:54:52 +0800

    update

--- utils/registry_factory.py
+++ utils/registry_factory.py
@@ -95,22 +95,29 @@ BENCH_REGISTRY = BenchRegister()
 
 if __name__ == "__main__":
 
-    def init_torch_linear(A_shape, B_shape, weight):
-        A = torch.randn(A_shape[0], A_shape[1], dtype=torch.float16, device="cuda")
+    def init_torch_linear(A_shape, B_shape, A_data, B_data):
+        assert A_shape == A_data.shape
+        assert B_shape == B_data.shape
         torch_linear = torch.nn.Linear(B_shape[1], B_shape[0], bias=False).cuda()
-        torch_linear.weight.data = weight
-        return {"A": A, "torch_linear": torch_linear}
+        torch_linear.weight.data = B_data
+        return {"A_data": A_data, "torch_linear": torch_linear}
 
-    def run_torch_linear(A, torch_linear):
-        torch_linear(A)
+    def run_torch_linear(A_data, torch_linear):
+        torch_linear(A_data)
 
     BENCH_REGISTRY.register("torch_linear", run_torch_linear, init_torch_linear)
 
     A_shape = (8192, 4096)
     B_shape = (4096, 4096)
-    weight = torch.randn(B_shape[1], B_shape[0], dtype=torch.float16, device="cuda")
+    A_data = torch.randn(A_shape[0], A_shape[1], dtype=torch.float16, device="cuda")
+    B_data = torch.randn(B_shape[1], B_shape[0], dtype=torch.float16, device="cuda")
     init_params = {
-        "default": {"A_shape": A_shape, "B_shape": B_shape, "weight": weight},
+        "default": {
+            "A_shape": A_shape,
+            "B_shape": B_shape,
+            "A_data": A_data,
+            "B_data": B_data,
+        },
         "torch_linear": {},
     }
 
