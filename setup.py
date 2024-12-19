f1e72c3dce4634853ad2c57c73322f17d3777f0a
helloyongyang <yongyang1030@163.com>
2024-12-19 20:25:06 +0800

    update setup

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
             sources=[
                 "kernels/fp6/deepseed/pybind_deepseed.cpp",
                 "kernels/fp6/deepseed/common/linear_kernels_cuda.cu",
-                "kernels/fp6/deepseed/common/linear_kernels.cpp"
+                "kernels/fp6/deepseed/common/linear_kernels.cpp",
             ],
             extra_compile_args=extra_compile_args,
         ),
@@ -60,6 +60,72 @@ setup(
                 "kernels/fp8/fp8_cuda_kernel.cu",
             ],
             extra_compile_args=extra_compile_args,
+        ),
+        CUDAExtension(
+            name="qserve_backend_qgemm_w4a8_per_group",
+            sources=[
+                "kernels/w4a8/qoq/csrc/qgemm/w4a8_per_group/pybind.cpp",
+                "kernels/w4a8/qoq/csrc/qgemm/w4a8_per_group/gemm_cuda.cu",
+            ],
+            extra_compile_args={
+                "cxx": [
+                    "-g",
+                    "-O3",
+                    "-fopenmp",
+                    "-lgomp",
+                    "-std=c++17",
+                    "-DENABLE_BF16",
+                ],
+                "nvcc": extra_compile_args["nvcc"]
+                + [
+                    "-O2",
+                    "-std=c++17",
+                    "-DENABLE_BF16",
+                    "-U__CUDA_NO_HALF_OPERATORS__",
+                    "-U__CUDA_NO_HALF_CONVERSIONS__",
+                    "-U__CUDA_NO_BFLOAT16_OPERATORS__",
+                    "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+                    "-U__CUDA_NO_BFLOAT162_OPERATORS__",
+                    "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
+                    "--expt-relaxed-constexpr",
+                    "--expt-extended-lambda",
+                    "--use_fast_math",
+                    "--threads=8",
+                ],
+            },
+        ),
+        CUDAExtension(
+            name="qserve_backend_qgemm_w4a8_per_chn",
+            sources=[
+                "kernels/w4a8/qoq/csrc/qgemm/w4a8_per_chn/pybind.cpp",
+                "kernels/w4a8/qoq/csrc/qgemm/w4a8_per_chn/gemm_cuda.cu",
+            ],
+            extra_compile_args={
+                "cxx": [
+                    "-g",
+                    "-O3",
+                    "-fopenmp",
+                    "-lgomp",
+                    "-std=c++17",
+                    "-DENABLE_BF16",
+                ],
+                "nvcc": extra_compile_args["nvcc"]
+                + [
+                    "-O2",
+                    "-std=c++17",
+                    "-DENABLE_BF16",
+                    "-U__CUDA_NO_HALF_OPERATORS__",
+                    "-U__CUDA_NO_HALF_CONVERSIONS__",
+                    "-U__CUDA_NO_BFLOAT16_OPERATORS__",
+                    "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+                    "-U__CUDA_NO_BFLOAT162_OPERATORS__",
+                    "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
+                    "--expt-relaxed-constexpr",
+                    "--expt-extended-lambda",
+                    "--use_fast_math",
+                    "--threads=8",
+                ],
+            },
         ),
     ],
     cmdclass={"build_ext": BuildExtension},
